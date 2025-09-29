c73263bb89484187c1a1c0380148a962ad7759a7
galuazz-droid <galuazz@gmail.com>
2025-09-29 04:48:03 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
+# app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'zhko-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///zhko.db'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'zhko-secret-key')
+    # Подключение к PostgreSQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'postgresql://zhko:zhko@db:5432/zhko'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
